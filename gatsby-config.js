092a3349feebae15a9d926dd73ab33c9fb3d20b8
marco-studio <60696119+marco-studio@users.noreply.github.com>
2020-02-12 09:28:37 +0000

    finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Over The Bridge`,
-    description: `Hey, meet Bilal, Kwaku, Patrick &amp; Tom. 4 Black &amp; Mixed-Race guys who became friends whilst studying at Cambridge University. Join us as we talk about life before, during and well - after 'The Bridge'. Expect chats about life, and our own experiences.
-    email: otbpodcastuk@gmail.com
-    Twitter: @otbpodcastuk
-    Cast: 
-    Kwaku: @KwakuDapaah_
-    Patrick: @CariocoLondrino
-    Bilal: @Tweetsbybilal
-    Tom: @TomTheEconomist`,
-    author: `Kwaku, Patrick, Bilal & Tom`,
-    twitter: `https://twitter.com/otbpodcastuk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor`,
-    instagram: `https://www.instagram.com/otbpodcastuk/`,
-    facebook: ``,
+    title: `Dynamic Power`,
+    description: `Co-Hosts Jeremy and Molly dated in High School, joined the military and ultimately met again at their 10 year High School Reunion. We raised a blended family and dealt with the trials and tribulations that come with parenthood. We found fitness and it has made an extreme impact on our mentality. Join us on our Transformation Journey as we shed our old habits, way of thinking, and old perspective as we intend to expand and gain new perspective. Our goal is to have you, the listener, learn from our experiences to know you are not alone and there can be something greater out there if you want it and will do what's necessary to get it. Power comes from the heart, mind and body. Each one of these harness their own strength. Aligning the heart, mind and body as one singular force will create a higher level of POWER. BE POWERFUL`,
+    author: `Jeremy & Molly Clark`,
+    twitter: ``,
+    instagram: `https://www.instagram.com/powerful_jeremy_clark/`,
+    facebook: `https://www.facebook.com/groups/teamdynamicpower/?ref=bookmarks`,
     youtube: ``,
-    apple: `https://podcasts.apple.com/podcast/id1364395842?ct=podlink&mt=2`,
-    stitcher: ``,
-    google: `https://podcasts.google.com/?feed=aHR0cDovL2ZlZWRzLnNvdW5kY2xvdWQuY29tL3VzZXJzL3NvdW5kY2xvdWQ6dXNlcnM6NDA3NTYwNzY0L3NvdW5kcy5yc3M=`,
-    pocket: `https://pca.st/itunes/1364395842`,
-    spotify: `https://open.spotify.com/show/0Lo2a3hwV0nxymHUj2lNIk`,
-    overcast: `https://overcast.fm/itunes1364395842`,
-    castbox: `https://soundcloud.com/overthebridgeuk`,
-    castro: `https://castro.fm/itunes/1364395842`,
-    podbean: `https://www.podbean.com/podcast-detail/g9v5c-68ac3/Over-The-Bridge-Podcast`,
-    beaker: `https://www.breaker.audio/shows?feed_url=http%3A%2F%2Ffeeds.soundcloud.com%2Fusers%2Fsoundcloud%3Ausers%3A407560764%2Fsounds.rss`
+    apple: `https://podcasts.apple.com/podcast/id1448789515?ct=podlink&mt=2`,
+    stitcher: `https://www.stitcher.com/s?fid=479019`,
+    google: `https://podcasts.google.com/?feed=aHR0cHM6Ly9keW5hbWljcG93ZXIubGlic3luLmNvbS9yc3M=`,
+    pocket: `https://pca.st/itunes/1448789515`,
+    spotify: `https://open.spotify.com/show/6fKDUsHGj26NTDuwKNDMZQ`,
+    overcast: `https://overcast.fm/itunes1448789515`,
+    castbox: `https://castbox.fm/vic/1448789515`,
+    castro: `https://castro.fm/itunes/1448789515`,
+    podbean: ``,
+    beaker: `https://www.breaker.audio/shows?feed_url=https%3A%2F%2Fdynamicpower.libsyn.com%2Frss`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -59,7 +52,7 @@ module.exports = {
         // url: `https://www.gatsbyjs.org/blog/rss.xml`,
         // url: `https://boysinthecave.com/feed/podcast/`,
         // url: `http://boysinthecave.libsyn.com/rss`,
-        url: `http://feeds.soundcloud.com/users/soundcloud:users:407560764/sounds.rss`,
+        url: `http://dynamicpower.libsyn.com/rss`,
         name: `GatsbyBlog`,
         // Optional
         // Read parser document: https://github.com/bobby-brennan/rss-parser#readme
